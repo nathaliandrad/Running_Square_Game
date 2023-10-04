a6c6210a4873e7230707206ab2b53b3e22e6c49e
Nathalia <47846610+nathaliandrad@users.noreply.github.com>
2023-10-04 08:17:42 +0000

    Update SetPosition.cs

--- Assets/Scripts/GameManager/SetPosition.cs
+++ Assets/Scripts/GameManager/SetPosition.cs
@@ -1,53 +1,42 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SetPosition : MonoBehaviour
 {
-    public float distance;
+    public float distance = 10.0f;
     public Transform targetObstacle;
-    float x, z, y;
     public GameObject invincible;
     public GameObject jump;
-    Vector3 pos;
-    public int randomNum;
+
+    private Vector3 pos;
+    private int randomNum;
 
     void Start()
     {
         randomNum = Random.Range(1, 3);
-        distance = 10.0f;
         JumpingOrInvincibleObj();
     }
 
-    public void SetPositionRandomly() {
-        x = Random.Range(-20, 20);
-        y = 1;
-        z = Random.Range(1000, 1500);
+    public void SetPositionRandomly()
+    {
+        float x = Random.Range(-20, 20);
+        float y = 1;
+        float z = Random.Range(1000, 1500);
         pos = new Vector3(x, y, z);
         transform.position = pos;
     }
 
-    public void JumpingOrInvincibleObj()
+    public void ToggleRendererAndCollider(GameObject obj, bool isEnabled)
     {
-        if(randomNum == 1)
-        {
-            jump.gameObject.GetComponent<Renderer>().enabled = false;
-            jump.gameObject.GetComponent<Collider>().enabled = false;
-            invincible.gameObject.GetComponent<Renderer>().enabled = true;
-            invincible.gameObject.GetComponent<Collider>().enabled = true;
-            SetPositionRandomly();
-        }
-        else if(randomNum == 2)
-        {
-            invincible.gameObject.GetComponent<Renderer>().enabled = false;
-            invincible.gameObject.GetComponent<Collider>().enabled = false;
-            jump.gameObject.GetComponent<Renderer>().enabled = true;
-            jump.gameObject.GetComponent<Collider>().enabled = true;
-            SetPositionRandomly();
-        }
- 
-
+        obj.GetComponent<Renderer>().enabled = isEnabled;
+        obj.GetComponent<Collider>().enabled = isEnabled;
     }
 
-
+    public void JumpingOrInvincibleObj()
+    {
+        ToggleRendererAndCollider(jump, randomNum == 2);
+        ToggleRendererAndCollider(invincible, randomNum == 1);
+        SetPositionRandomly();
+    }
 }
